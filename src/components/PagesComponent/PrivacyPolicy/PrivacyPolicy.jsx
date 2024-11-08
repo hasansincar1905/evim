1a5a45d75f05d42eca29e098fefec1135e5c65c8
ziadm7071 <ziad1090mohamed@gmail.com>
2024-11-08 21:12:17 +0200

    fq

--- src/components/PagesComponent/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/components/PagesComponent/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,28 +1,53 @@
-'use client'
-import React from 'react'
-import BreadcrumbComponent from '@/components/Breadcrumb/BreadcrumbComponent'
-import { store } from '@/redux/store'
-import { t } from '@/utils'
+"use client";
+import React, { useEffect, useState } from "react";
+import BreadcrumbComponent from "@/components/Breadcrumb/BreadcrumbComponent";
+import { store } from "@/redux/store";
+import { t } from "@/utils";
+import { settingTranslationApi } from "@/utils/api";
+import { CurrentLanguageData } from "@/redux/reuducer/languageSlice";
+import { useSelector } from "react-redux";
 
 const PrivacyPolicy = () => {
+  const settingsData = store.getState().Settings.data;
+  const privacy = settingsData?.data?.privacy_policy;
+  const [PrivacyPolicyData, setPrivacyPolicyData] = useState("");
 
-    const settingsData = store.getState().Settings.data
-    const privacy = settingsData?.data?.privacy_policy
-    return (
-        <section className='aboutus'>
-            <BreadcrumbComponent title2={t('privacyPolicy')} />
-            <div className='container'>
-                {/* <div className="main_title">
+  const language = useSelector(CurrentLanguageData);
+  const getSettingTranslationData = async () => {
+    try {
+      const response = await settingTranslationApi.getSettingTranslation({
+        setting_id: "privacy_policy",
+      });
+      const { data } = response.data;
+      if (data) {
+        setPrivacyPolicyData(data.value);
+      }
+
+      console.log("PrivacyPolicyData", data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    getSettingTranslationData();
+  }, [language]);
+
+  return (
+    <section className="aboutus">
+      <BreadcrumbComponent title2={t("privacyPolicy")} />
+      <div className="container">
+        {/* <div className="main_title">
                     <span>
                         {t('aboutUs')}
                     </span>
                 </div> */}
-                <div className="page_content">
-                    <div dangerouslySetInnerHTML={{ __html: privacy || "" }} />
-                </div>
-            </div>
-        </section>
-    )
-}
+        <div className="page_content">
+          <div dangerouslySetInnerHTML={{ __html: PrivacyPolicyData || "" }} />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default PrivacyPolicy
+export default PrivacyPolicy;
--- src/components/PagesComponent/TermsAndCondition/TermsAndCondition.jsx
+++ src/components/PagesComponent/TermsAndCondition/TermsAndCondition.jsx
@@ -1,28 +1,52 @@
-'use client'
-import React from 'react'
-import BreadcrumbComponent from '@/components/Breadcrumb/BreadcrumbComponent'
-import { store } from '@/redux/store'
-import { t } from '@/utils'
+"use client";
+import React, { useEffect, useState } from "react";
+import BreadcrumbComponent from "@/components/Breadcrumb/BreadcrumbComponent";
+import { store } from "@/redux/store";
+import { t } from "@/utils";
+import { settingTranslationApi } from "@/utils/api";
+import { useSelector } from "react-redux";
+import { CurrentLanguageData } from "@/redux/reuducer/languageSlice";
 
 const TermsAndCondition = () => {
+  const settingsData = store.getState().Settings.data;
+  const privacy = settingsData?.data?.terms_conditions;
 
-    const settingsData = store.getState().Settings.data
-    const privacy = settingsData?.data?.terms_conditions
-    return (
-        <section className='aboutus'>
-            <BreadcrumbComponent title2={t('termsConditions')} />
-            <div className='container'>
-                {/* <div className="main_title">
+  const [TermsData, setTermsData] = useState("");
+
+  const language = useSelector(CurrentLanguageData);
+  const getSettingTranslationData = async () => {
+    try {
+      const response = await settingTranslationApi.getSettingTranslation({
+        setting_id: "terms_conditions",
+      });
+      const { data } = response.data;
+      if (data) {
+        setTermsData(data.value);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    getSettingTranslationData();
+  }, [language]);
+
+  return (
+    <section className="aboutus">
+      <BreadcrumbComponent title2={t("termsConditions")} />
+      <div className="container">
+        {/* <div className="main_title">
                     <span>
                         {t('aboutUs')}
                     </span>
                 </div> */}
-                <div className="page_content">
-                    <div dangerouslySetInnerHTML={{ __html: privacy || "" }} />
-                </div>
-            </div>
-        </section>
-    )
-}
+        <div className="page_content">
+          <div dangerouslySetInnerHTML={{ __html: TermsData || "" }} />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default TermsAndCondition
+export default TermsAndCondition;
